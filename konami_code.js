f92b5c13d7f2707d291ee0e7246c913674fb7c67
Mark Baker <mrbaker1917@gmail.com>
2018-07-26 03:31:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -21,7 +21,7 @@ function init() {
     if (key === codes[index]) {
       index++;
       if (index === codes.length) {
-        alert("Hurray!");
+        window.alert("Hurray!");
         
         index = 0;
       }
